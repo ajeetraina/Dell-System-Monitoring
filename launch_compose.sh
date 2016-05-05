c6db1b91b145c2b4a9aa474af7791c979bb7cc48
Ajeet Singh Raina <ajeetraina@gmail.com>
2016-05-05 22:36:50 +0530

    Update launch_compose.sh

--- launch_compose.sh
+++ launch_compose.sh
@@ -5,4 +5,4 @@ GENERATED_FILE=compose_generated.yml
 ENV_DEPENDENT_MODULE_FOLDER=/lib/modules/$(uname -r):/lib/modules/$(uname -r)
 sed "s;{{MODULE_FOLDER}};${ENV_DEPENDENT_MODULE_FOLDER};g" < $INPUT_FILE > $GENERATED_FILE
 
-docker-compose  -f $GENERATED_FILE  up 
+docker-compose  -f $GENERATED_FILE  up -d
